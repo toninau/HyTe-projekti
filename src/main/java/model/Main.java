eaa42f4bf16cc461627be4ba6a9d4e0731e69cc1
Toni Naumanen <toninau@metropolia.fi>
2019-02-12 13:38:54 +0200

    testaa kaikkien asiakkaiden luku

--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -5,7 +5,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		AsiakasAccessObject asiakasDAO = new AsiakasAccessObject();
+		// Lue yksittäinen asiakas
 		Asiakas asiakas = asiakasDAO.readAsiakas("101150-010A");
 		System.out.println(asiakas.getEtunimi());
+		// Lue kaikki asiakkaat
+		Asiakas[] asiakkaat = asiakasDAO.readAsiakkaat();
+		for (Asiakas a : asiakkaat) {
+			System.out.println(a.getEtunimi());
+		}
 	}
 }
