fa344e35a9c2bbc86d2e694549fc48dfb0553304
Toni Naumanen <toninau@metropolia.fi>
2019-02-14 13:19:57 +0200

    asiakas testin lisäys

---
+++ src/test/java/model/AsiakasTest.java
@@ -0,0 +1,18 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class AsiakasTest {
+	private Asiakas asiakas;
+	
+	@Test
+	public void testAsiakasTyhjäKonstruktori() {
+		try {
+			 asiakas = new Asiakas();
+		} catch (Exception e) {
+			fail("Asiakas-olion luonti epäonnistui");
+		}
+	}
+}
