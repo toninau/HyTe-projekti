fa36f438644e4b68b56c22022bb8092326a18ef2
Ida Kilpeläinen <ida.kilpelainen@metropolia.fi>
2019-03-04 21:47:24 +0200

    changed skene.fxml and HyteGUI, added view.css and GUI tests

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,15 +1,14 @@
 package controller;
 
-import model.Model;
-import model.Model_IF;
+
 import view.HyteGUI_IF;
 
 public class Controller implements Controller_IF {
-	HyteGUI_IF gui;
-	Model_IF model;
+	/*HyteGUI_IF gui;
+
 	public Controller(HyteGUI_IF gui) {
 		this.gui = gui;
-		model = new Model();
-	}
+		
+	}*/
 
 }
