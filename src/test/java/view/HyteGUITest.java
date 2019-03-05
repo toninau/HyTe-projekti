b4cbda410065d5b91a1d6167b6850905ad90e3fc
Ida Kilpeläinen <ida.kilpelainen@metropolia.fi>
2019-03-05 15:26:39 +0200

    trying to get gui test working with jenkins

--- src/test/java/view/HyteGUITest.java
+++ src/test/java/view/HyteGUITest.java
@@ -1,34 +1,23 @@
 package view;
 
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.loadui.testfx.GuiTest;
-import org.testfx.api.FxAssert;
-import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationTest;
-import org.testfx.framework.junit5.Start;
 
 import org.testfx.api.FxToolkit;
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.matcher.control.LabeledMatchers.hasText;
-import static org.junit.Assert.*;
 
 public class HyteGUITest extends ApplicationTest{
 
@@ -63,13 +52,13 @@ public class HyteGUITest extends ApplicationTest{
     public void testInput() {
         clickOn("#username");
         write("name");
-       //assertThat(gui.getUsername(), is("name"));
+       
     }
+
     
     @Test
     public void testButton() {
     	Label label = (Label)GuiTest.find("#label");
-
         clickOn("#loginBtn");
         verifyThat(label, hasText("Welcome"));
     }
