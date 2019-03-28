8bacf6158f550354e06d09a9027197dd19c7dd0c
Jeremias Korhonen <jeremias.korhonen@live.fi>
2019-03-28 17:35:44 +0200

    Customer view redone, calendar tab nearly done, weather update

--- src/main/java/model/WeatherAPICall.java
+++ src/main/java/model/WeatherAPICall.java
@@ -5,7 +5,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 import java.net.HttpURLConnection;
-
+import java.net.MalformedURLException;
 import java.net.URL;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -20,20 +20,49 @@ import com.google.gson.JsonParser;
  * Käyttää GSON-kirjastoa JSON:in parsetukseen
  */
 public class WeatherAPICall {
+	private String celsius;
+	private String state;
+	
+	public WeatherAPICall() throws Exception{
+		String url = "http://api.openweathermap.org/data/2.5/weather?q=Helsinki&APPID=aec9142965fb3ca48cf10cd6300423fc";
+		
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		Gson gson = new GsonBuilder().create();
+
+		JsonElement jelement = new JsonParser().parse(response.toString());
+		JsonObject jobject = jelement.getAsJsonObject();
+		JsonArray statemain = jobject.getAsJsonArray("weather");
+		JsonObject celsiusmain = jobject.get("main").getAsJsonObject();
+		state = statemain.get(0).getAsJsonObject().get("main").toString();
+		celsius = celsiusmain.get("temp").toString();
+		System.out.println(celsius);
+		System.out.println(state);
+	}
+	/**public static void main(String[] args) throws Exception {
 
 		WeatherAPICall http = new WeatherAPICall();
 		System.out.println("Testing GET");
-		http.sendGet();
-
-	}
+		//http.sendGet();
+		http.getCelsius();
+	}**/
 	/**
 	 * OpenWeatherMap-API kutsuu halutun kaupungin sään
 	 * Tulostaa säätilan
 	 * @throws Exception virhe tapahtuu
 	 */
-	private void sendGet() throws Exception {
+	/**public String getState() throws Exception {
 
 		String url = "http://api.openweathermap.org/data/2.5/weather?q=Helsinki&APPID=aec9142965fb3ca48cf10cd6300423fc";
 		
@@ -54,7 +83,19 @@ public class WeatherAPICall {
 
 		JsonElement jelement = new JsonParser().parse(response.toString());
 		JsonObject jobject = jelement.getAsJsonObject();
-		JsonArray main = jobject.getAsJsonArray("weather");
-		System.out.println(main.get(0).getAsJsonObject().get("main"));
+		JsonArray statemain = jobject.getAsJsonArray("weather");
+		JsonObject celsiusmain = jobject.get("main").getAsJsonObject();
+		state = statemain.get(0).getAsJsonObject().get("main").toString();
+		celsius = celsiusmain.get("temp").toString();
+		//System.out.println(jobject);
+		//System.out.println(main.get(0).getAsJsonObject().get("main").toString());
+	}**/
+
+	public String getCelsius() {
+		return celsius;
+	}
+
+	public String getState() {
+		return state;
 	}
 }
--- src/main/java/view/CustomerView.java
+++ src/main/java/view/CustomerView.java
@@ -2,29 +2,110 @@ package view;
 
 import java.io.IOException;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+import model.WeatherAPICall;
 
 public class CustomerView extends ViewChanger {
 	
-	@FXML Text city;
-	@FXML Text weather;
+	@FXML Text weatherCity;
+	@FXML Text weatherState;
+	@FXML Text weatherCelsius;
 	@FXML TextArea notification;
-	@FXML Text welcome;
-	@FXML Text celcius;
+	@FXML Text welcomeText;
 	@FXML Button logout;
+	@FXML Button mondaybutton;
+	@FXML Button tuesdaybutton;
+	@FXML Button wednesdaybutton;
+	@FXML HBox mondaytextarea;
+	@FXML HBox tuesdaytextarea;
+	@FXML HBox wednesdaytextarea;
+	@FXML HBox thursdaytextarea;
+	@FXML HBox fridaytextarea;
+	@FXML HBox saturdaytextarea;
+	@FXML HBox sundaytextarea;
 	
-	public void setCelcius() {
+	
+	/**public void setCelcius() {
 		this.celcius.setText(null);
-	}
+	}**/
 	
 	public void logout(MouseEvent event) throws IOException {
 		String fxml = "/LoginView.fxml";
 		String title  = "Login";
 		sceneContent(fxml, event, title);
 	}
+	
+	public void openbox(MouseEvent event) throws IOException {
+		Button btn = (Button) event.getSource();
+		String id = btn.getId();
+		mondaytextarea.setPrefWidth(200);
+		tuesdaytextarea.setPrefWidth(200);
+		wednesdaytextarea.setPrefWidth(200);
+		thursdaytextarea.setPrefWidth(200);
+		fridaytextarea.setPrefWidth(200);
+		saturdaytextarea.setPrefWidth(200);
+		sundaytextarea.setPrefWidth(200);
+		
+		
+		new AnimationTimer() {
+			private long sleepNanoseconds = 15 * 1000000;
+            private long prevTime = 0;
+
+            public void handle(long currentNanoTime) {
+
+                if ((currentNanoTime - prevTime) < sleepNanoseconds) {
+                    return;
+                }
+                if (id.equals("mondaybutton")) {
+                	mondaytextarea.setPrefWidth(mondaytextarea.getPrefWidth()+10);
+                }
+                if (id.equals("tuesdaybutton")) {
+                	tuesdaytextarea.setPrefWidth(tuesdaytextarea.getPrefWidth()+10);
+                }
+                if (id.equals("wednesdaybutton")) {
+                	wednesdaytextarea.setPrefWidth(wednesdaytextarea.getPrefWidth()+10);
+                }
+                if (id.equals("thursdaybutton")) {
+                	thursdaytextarea.setPrefWidth(thursdaytextarea.getPrefWidth()+10);
+                }
+                if (id.equals("fridaybutton")) {
+                	fridaytextarea.setPrefWidth(fridaytextarea.getPrefWidth()+10);
+                }
+                if (id.equals("saturdaybutton")) {
+                	saturdaytextarea.setPrefWidth(saturdaytextarea.getPrefWidth()+10);
+                }
+                if (id.equals("sundaybutton")) {
+                	sundaytextarea.setPrefWidth(sundaytextarea.getPrefWidth()+10);
+                }
+   
+                prevTime = currentNanoTime;
+                if (mondaytextarea.getPrefWidth()>=400 || tuesdaytextarea.getPrefWidth()>=400 ||
+                		wednesdaytextarea.getPrefWidth()>=400 || thursdaytextarea.getPrefWidth()>=400 ||
+                		fridaytextarea.getPrefWidth()>=400 || saturdaytextarea.getPrefWidth()>=400 ||
+                		sundaytextarea.getPrefWidth()>=400) {
+                	stop();
+                }
+            }
+		}.start();
+	}
+	
+	public void initialize() {
+		try {
+			WeatherAPICall weather = new WeatherAPICall();
+			weatherState.setText(weather.getState());
+			weatherCelsius.setText(weather.getCelsius());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
 	
 }
