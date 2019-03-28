a4dda3a6f5613c720c1fd3726d929ed03c284bf6
mikkopuustinen <mikkowr@gmail.com>
2019-03-28 16:19:45 +0200

    edit staff view

---
+++ src/main/java/view/EditStaffView.java
@@ -0,0 +1,27 @@
+package view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class EditStaffView extends ViewChanger implements Initializable {
+
+	
+	
+	
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
