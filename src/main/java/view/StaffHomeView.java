7ecd41d50b5f9033aa99d2e18e4fc6ae162dfe5e
Jeremias Korhonen <jeremias.korhonen@live.fi>
2019-04-15 14:01:36 +0300

    Internationalized string for Staff home view

--- src/main/java/view/StaffHomeView.java
+++ src/main/java/view/StaffHomeView.java
@@ -7,12 +7,15 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 
 public class StaffHomeView extends ViewChanger implements Initializable{
 
-	
-	
+	@FXML ListView dailyNews;
+	@FXML ListView StaffCustomers;
+	@FXML Label welcomeText;
 	
 	@FXML
 	private Button logout;
