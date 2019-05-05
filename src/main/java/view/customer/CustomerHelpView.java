e3ac2b445a3a07ca1b3d5bf48e04e3aaca80f6e6
Toni Naumanen <toninau@metropolia.fi>
2019-05-05 14:33:05 +0300

    added javadoc to CustomerHelpView

--- src/main/java/view/customer/CustomerHelpView.java
+++ src/main/java/view/customer/CustomerHelpView.java
@@ -20,20 +20,34 @@ import view.ViewChanger;
 import view.enums.Bundles;
 
 public class CustomerHelpView extends ViewChanger implements Initializable {
-	
-	@FXML private ImageView slideShow;
-	@FXML private Button previousButton;
-    @FXML private Tooltip previousTT;
-	@FXML private Button nextButton;
-    @FXML private Tooltip nextTT;
-	@FXML private Button startButton;
-    @FXML private Tooltip startTT;
-	@FXML private Button homeButton1;
-	@FXML private Button myHealthButton1;
-	@FXML private Button helpButton1;
-    @FXML private Label helpText;
-    @FXML private Label helpCounter;
-	
+
+	@FXML
+	private ImageView slideShow;
+	@FXML
+	private Button previousButton;
+	@FXML
+	private Tooltip previousTT;
+	@FXML
+	private Button nextButton;
+	@FXML
+	private Tooltip nextTT;
+	@FXML
+	private Button startButton;
+	@FXML
+	private Tooltip startTT;
+	@FXML
+	private Button homeButton1;
+	@FXML
+	private Button myHealthButton1;
+	@FXML
+	private Button helpButton1;
+	@FXML
+	private Label helpText;
+	@FXML
+	private Label helpCounter;
+	@FXML
+	private Label helpTip;
+
 	private ResourceBundle bundle;
 	private List<String> imageList;
 	private List<String> textList;
@@ -45,6 +59,13 @@ public class CustomerHelpView extends ViewChanger implements Initializable {
 		textList = new ArrayList<>();
 	}
 
+	/**
+	 * Sets the right bundle. Calls the needed methods.
+	 * 
+	 * @see #fillImageList()
+	 * @see #fillTextList()
+	 * @see #openImage()
+	 */
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		bundle = ResourceBundle.getBundle(Bundles.CUSTOMER.getBundleName(), HyteGUI.getLocale());
@@ -55,33 +76,51 @@ public class CustomerHelpView extends ViewChanger implements Initializable {
 		fillTextList();
 		openImage();
 	}
-	
+
+	/**
+	 * Returns to the beginning.
+	 * 
+	 * @param event ActionEvent
+	 */
 	@FXML
 	private void startImage(ActionEvent event) {
 		index = 0;
 		openImage();
 	}
 
+	/**
+	 * Changes image to next image.
+	 * 
+	 * @param event ActionEvent
+	 */
 	@FXML
 	private void nextImage(ActionEvent event) {
-		if (index < imageList.size()-1) {
+		if (index < imageList.size() - 1) {
 			index++;
 		} else {
 			index = 0;
 		}
 		openImage();
 	}
-	
+
+	/**
+	 * Changes image to previous image.
+	 * 
+	 * @param event ActionEvent
+	 */
 	@FXML
 	private void previousImage(ActionEvent event) {
 		if (index > 0) {
 			index--;
 		} else {
-			index = imageList.size()-1;
+			index = imageList.size() - 1;
 		}
 		openImage();
 	}
-	
+
+	/**
+	 * Sets image to ImageView. Also sets help text below image and image counter.
+	 */
 	private void openImage() {
 		try {
 			File file = new File(CustomerHelpView.class.getResource(imageList.get(index)).getFile());
@@ -89,21 +128,30 @@ public class CustomerHelpView extends ViewChanger implements Initializable {
 			Image image = new Image(test);
 			slideShow.setImage(image);
 			helpText.setText(textList.get(index));
-			helpCounter.setText(index + 1 + "/" + imageList.size());
-		} catch (Exception e) {}
+			int index1 = index + 1;
+			helpCounter.setText(index1 + "/" + imageList.size());
+			helpTip.setText(bundle.getString("help.mainTip") + " " + index1);
+		} catch (Exception e) {
+		}
 	}
 
+	/**
+	 * Fills list (imageList) with helpful images.
+	 */
 	private void fillImageList() {
 		for (int i = 1; i < 4; i++) {
 			imageList.add("/pictures/" + i + ".jpg");
 		}
 	}
-	
+
+	/**
+	 * Fills list (textList) with tips that are shown below images.
+	 */
 	private void fillTextList() {
 		for (int i = 0; i < imageList.size(); i++) {
 			textList.add(bundle.getString("help.tip" + i));
 		}
-	}	
+	}
 
 	/**
 	 * Fired when Home button is clicked.
